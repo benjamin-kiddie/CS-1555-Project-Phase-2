75d0d5c7f01b0802c1fc27b0352b3392123fdef4
Ethan <etw15@pitt.edu>
2023-11-12 19:11:48 -0500

    Created operations 2-5

--- operations.sql
+++ operations.sql
@@ -22,3 +22,37 @@ CREATE OR REPLACE PROCEDURE addForest(n varchar(30), a integer, al real, xmin re
     $$ LANGUAGE plpgsql;
 
 CALL addForest('Test 2', 513655, 0, 0, 20, 0, 20);
+
+CREATE OR REPLACE PROCEDURE addTreeSpecies(gen varchar(30), epi varchar(30), temp real, height real, raunkiaer raunkiaer_life_form) AS
+    $$
+    BEGIN
+        INSERT INTO TREE_SPECIES
+        VALUES (gen, epi, temp, height, raunkiaer);
+    END;
+    $$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE PROCEDURE addSpeciesToForest(no integer, gen varchar(30), epi varchar(30)) AS
+    $$
+    BEGIN
+        INSERT INTO FOUND_IN
+        VALUES (no, gen, epi);
+    END;
+    $$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE PROCEDURE newWorker(ssn char(9), f varchar(30), l varchar(30), mi char(1), r rank, abb char(2)) AS
+    $$
+    BEGIN
+        INSERT INTO WORKER
+        VALUES (ssn, f, l, mi, r);
+        INSERT INTO EMPLOYED
+        VALUES (abb, ssn);
+    END;
+    $$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE PROCEDURE employWorkerToState(ssn char(9), abb char(2)) AS
+    $$
+    BEGIN
+        INSERT INTO EMPLOYED
+        VALUES (abb, ssn);
+    END;
+    $$ LANGUAGE plpgsql;
