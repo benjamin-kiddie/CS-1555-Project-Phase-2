86024818070015e675a7a94b8950628990e97c73
Benjamin Kiddie <benkiddie009@gmail.com>
2023-11-17 13:35:04 -0500

    Implement rankForestSensors()

--- analytical.sql
+++ analytical.sql
@@ -4,3 +4,34 @@
 --
 -- Authors: Hala Nubani, Ethan Wells, Ben Kiddie
 ------------------------------------------------
+
+SET SCHEMA 'arbor_db';
+
+-- View that shows the number of sensors in a forest.
+-- Used by rankForestSensors().
+DROP VIEW IF EXISTS numSensorsInForest;
+CREATE VIEW numSensorsInForest AS
+    SELECT i.forest_no, COUNT(forest_no) AS num_sensors
+    FROM (SELECT forest_no, sensor_id
+          FROM SENSOR s
+          JOIN FOREST f ON (s.x BETWEEN f.mbr_xmin AND f.mbr_xmax)
+                            AND (s.y BETWEEN f.mbr_ymin AND f.mbr_ymax)) AS i
+    GROUP BY i.forest_no;
+
+-- Rank all forests based on the number of sensors within them,
+-- with forests with more sensors being ranked higher than
+-- forests with fewer sensors.
+CREATE OR REPLACE FUNCTION rankForestSensors() RETURNS TABLE (
+        forest_no integer,
+        rank bigint
+    ) AS
+    $$
+    BEGIN
+        RETURN QUERY
+            SELECT n.forest_no, RANK () OVER (
+                ORDER BY n.num_sensors DESC
+            )
+        FROM numSensorsInForest n;
+    END;
+    $$ LANGUAGE plpgsql;
+
--- tests.sql
+++ tests.sql
@@ -7,33 +7,5 @@
 
 SET SCHEMA 'arbor_db';
 
--- Testing of operations:
-INSERT INTO CLOCK VALUES (NOW());
-INSERT INTO STATE VALUES ('Rhode Island', 'RI', 0, 1, 0, 20, 0, 20);
-INSERT INTO STATE VALUES ('New York', 'NY', 0, 1, 21, 40, 21, 40);
-INSERT INTO STATE VALUES ('New York 2', 'NL', 0, 1, 21, 40, 21, 40);
-CALL addForest('Test Forest', 0, 7, 0, 20, 0, 20);
-CALL addTreeSpecies('Test', 'Species', 100, 7, 'Epiphytes');
-CALL addSpeciesToForest(1, 'Test', 'Species');
-CALL newWorker('000000000', 'Test', 'Test', 'T', 'Associate', 'RI');
-CALL newWorker('000000001', 'Test2', 'Test2', 'T', 'Associate', 'RI');
-CALL employWorkerToState('000000000', 'NY');
-CALL placeSensor(100, 0, 0, '000000000');
-CALL generateReport(1, LOCALTIMESTAMP, 100);
-
-CALL removeSpeciesFromForest(1, 'Test', 'Species');
-CALL deleteWorker('000000000');
-CALL moveSensor(1, 100, 100);
-
-SELECT MIN(worker)
-FROM EMPLOYED
-WHERE state = 'RI' AND worker != '000000000';
-UPDATE sensor
-SET maintainer_id = '000000000'
-WHERE sensor_id = 1;
-CALL employWorkerToState('000000000', 'RI');
-CALL removeWorkerFromState('000000000', 'RI');
-
-SELECT * FROM listSensors(1);
-SELECT * FROM listMaintainedSensors('873331998');
-SELECT * FROM locateTreeSpecies('Test', 'Species');
+-- Testing of analytical queries.
+SELECT * FROM rankForestSensors();
